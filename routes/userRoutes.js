b8a0738a1176ef93636d3d856c0357db1f26b217
Naresh Chudasama <nareshchudasama@knovator.in>
2023-04-06 14:36:41 +0530

    validation add in routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const route = express.Router();
 const { updateUser, deleteUser, createAlbumOfUser, getAlbumOfUser } = require("../controller/userController");
+const { userValidate, albumValidate } = require("../validation/validate")
 
-route.get("/album", getAlbumOfUser)  // API_1  
-route.post("/album", createAlbumOfUser)  // API_2
-route.put("/:id", updateUser) // API_3
-route.delete("/:id", deleteUser) // API_4
+route.get("/album", userValidate, albumValidate, getAlbumOfUser)  // API_1  
+route.post("/album", userValidate, albumValidate, createAlbumOfUser)  // API_2
+route.put("/:id", userValidate, albumValidate, updateUser) // API_3
+route.delete("/:id", userValidate, albumValidate, deleteUser) // API_4
 
 module.exports = route;
