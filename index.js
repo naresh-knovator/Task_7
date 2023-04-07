c6692c952275eec5f71f87d354a0a9df87e41bf0
Naresh Chudasama <nareshchudasama@knovator.in>
2023-04-07 16:26:18 +0530

    rateLimit add

--- index.js
+++ index.js
@@ -12,7 +12,19 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use("/api/user", userRoutes)
-
+const rateLimit = require("express-rate-limit");
+const limiter = rateLimit({
+    max: 2,
+    windowMs: 60 * 60 * 1000,
+    message: "Too many request from this IP"
+});
+app.use(limiter);
+app.get("/", (req, res) => {
+    res.status(200).json({
+        status: "success",
+        message: "Hello from the express server"
+    });
+});
 app.use(notFound)
 app.use(errorHandle)
 
